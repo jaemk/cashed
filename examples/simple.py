357a729d4717850c202274cc3869ed888d1e6b43
James Kominick <james.kominick@gmail.com>
2017-03-26 17:54:28 -0400

    update examples

--- examples/simple.py
+++ examples/simple.py
@@ -2,18 +2,48 @@ import time
 from itertools import chain, repeat
 from cashed import cached
 
+
 pairs = [('james', None),
          ('james', 'bean'),
          ('bob', 'bill'),
          ('lauren', 'bailey'),
          ('lauren', None),]
 
+
 @cached(capacity=10, seconds=30)
-def slow(name, pet_name=None):
+def slow_op(name, pet_name=None):
     time.sleep(1)
     pet = pet_name if pet_name else 'no pet!'
     return '{name} : {pet}'.format(name=name, pet=pet)
 
+
+print('** Examples 1 **')
 for name, pet in chain(*repeat(pairs, 3)):
-    ans = slow(name, pet_name=pet)
-    print(ans)
+    start = time.time()
+    ans = slow_op(name, pet_name=pet)
+    print('{} -> {}'.format(time.time()-start, ans))
+
+
+######
+
+
+def fib_slow(n):
+    if n == 0 or n == 1:
+        return n
+    return fib_slow(n-1) + fib_slow(n-2)
+
+
+@cached(capacity=100)
+def fib_fast(n):
+    if n == 0 or n == 1:
+        return n
+    return fib_fast(n-1) + fib_fast(n-2)
+
+
+print('** Examples 2 **')
+for _type, func in [('uncached', fib_slow), ('cached', fib_fast)]:
+    start = time.time()
+    for n in range(30):
+        v = func(n)
+    print(' ** {} -> {}s **'.format(_type, time.time()-start))
+
